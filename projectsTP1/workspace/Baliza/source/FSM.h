2909c940be43e326c238d97ced23deb738198cff
LuFernandez <lu.gfernandez@gmail.com>
2019-08-28 20:18:22 -0300

    cambie unas cositas en el fsm.h

--- projectsTP1/workspace/Baliza/source/FSM.h
+++ projectsTP1/workspace/Baliza/source/FSM.h
@@ -12,22 +12,22 @@
 #define FSM_H_
 
 //Defincion de constantes y parametros
-#define NUM_TIMERS 2 //Numero de timers que se utilizan.
+//#define NUM_TIMERS 2 //Numero de timers que se utilizan.
 #define ID_LENGTH 8 //Longitud maxima del ID del usuario.
 #define PIN_LENGTH 5 //Longitud maxima del PIN del usuario.
-#define NUM_EVENTS 3  //Cantidad de eventos posibles.
+//#define NUM_EVENTS 3  //Cantidad de eventos posibles.
 
-enum timers{INACTIVITY, DISPLAY}; //Timers utilizados
+enum timers{INACTIVITY, DISPLAY, NUM_TIMERS}; //Timers utilizados
 enum states{WAITING_ID, RECEIVING_ID, RECEIVING_PIN, ACCESS_GRANTED, ADMIN_MODE, ADDING_USER,
-			REMOVING_USER, CHANGING_PIN}; //Estados posibles de la FSM
-enum events{INPUT_EV, TIMER_EV, KEYCARD_EV};
+			REMOVING_USER, CHANGING_PIN, NUM_STATES}; //Estados posibles de la FSM
+enum events{INPUT_EV, TIMER_EV, KEYCARD_EV, NUM_EVENTS};
 
 
 //UserData contiene toda la informacion necesaria que necesita una rutina para manejar
 //un evento que se saco de la cola.
 typedef struct{
-   char recieved_ID[ID_LENGTH]; //String con el ID recibido del usuario.
-   char recieved_PIN[PIN_LENGTH]; //String con el PIN recibido del usuario.
+   char received_ID[ID_LENGTH]; //String con el ID recibido del usuario.
+   char received_PIN[PIN_LENGTH]; //String con el PIN recibido del usuario.
    bool timers[NUM_TIMERS]; //Arreglo que indica que timers expiraron.
 }UserData_t;
 
