b750693e7899932d427aba1142162b35bd5a6f17
EzequielVijande <evijande@itba.edu.ar>
2019-08-28 13:53:01 -0300

    Agregue el FSM.h

---
+++ projectsTP1/workspace/Baliza/source/App.c
@@ -0,0 +1,97 @@
+/***************************************************************************//**
+  @file     App.c
+  @brief    Application functions
+  @author   Nicolás Magliola
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "board.h"
+#include "gpio.h"
+#include "FSM.h"
+
+#define DELAY_BASE 900000UL
+#define DELAY_LIMIT_CONST 10
+
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static void delayLoop(uint32_t veces);
+_Bool buttonLastState = false;
+_Bool isBalizaOn = false;
+int delayLimit = DELAY_LIMIT_CONST;
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+/* Función que se llama 1 vez, al comienzo del programa */
+void App_Init (void)
+{
+    gpioMode(PIN_LED_RED, OUTPUT);
+    //gpioMode(PIN_LED_BLUE, OUTPUT);
+    gpioMode(PIN_SW3 , SW_INPUT_TYPE);
+    //gpioMode(PIN_SW2 , SW_INPUT_TYPE);
+    gpioMode(PIN_LED_EXTERNAL , EXTERNAL_LED_TYPE);
+    //gpioMode(PIN_SWITCH_EXTERNAL , EXTERNAL_SW_TYPE);
+    gpioWrite(PIN_LED_RED, HIGH);
+}
+
+/* Función que se llama constantemente en un ciclo infinito */
+void App_Run (void)
+{
+
+	_Bool buttonResult = !gpioRead(PIN_SW3); //SW3 es  activo bajo, esta presionado cuando se lee cero lógico
+	if((buttonResult != (buttonLastState)) && (buttonResult) ) //Se ingresa al bloque si se presionó el botón.
+	{
+		isBalizaOn = !isBalizaOn;
+		gpioToggle(PIN_LED_RED);
+		if (isBalizaOn)
+		{
+			gpioWrite(PIN_LED_EXTERNAL, HIGH); //que la baliza arranque prendida
+		}
+		else
+		{
+			gpioWrite(PIN_LED_EXTERNAL, LOW); //que la baliza se apague inmediatamente
+		}
+	}
+	buttonLastState = buttonResult;
+	if (isBalizaOn)
+	{
+
+		delayLoop(DELAY_BASE);
+		if (!(delayLimit--))
+		{
+			gpioToggle(PIN_LED_EXTERNAL);
+			delayLimit = DELAY_LIMIT_CONST;
+		}
+
+	}
+}
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+static void delayLoop(uint32_t veces)
+{
+    while ((veces--)) asm("nop");
+}
+
+
+/*******************************************************************************
+ ******************************************************************************/
---
+++ projectsTP1/workspace/Baliza/source/FSM.h
@@ -0,0 +1,48 @@
+/*
+ * FSM.h
+ *
+ * Header con las definiciones de las estructuras utilizadas
+ * para los  estados, eventos y la FSM.
+ *
+ *  Created on: Aug 28, 2019
+ *      Author: Ezequiel Vijande
+ */
+
+#ifndef FSM_H_
+#define FSM_H_
+
+//Defincion de constantes y parametros
+#define NUM_TIMERS 2 //Numero de timers que se utilizan.
+#define ID_LENGTH 8 //Longitud maxima del ID del usuario.
+#define PIN_LENGTH 5 //Longitud maxima del PIN del usuario.
+#define NUM_EVENTS 3  //Cantidad de eventos posibles.
+
+enum timers{INACTIVITY, DISPLAY}; //Timers utilizados
+enum states{WAITING_ID, RECEIVING_ID, RECEIVING_PIN, ACCESS_GRANTED, ADMIN_MODE, ADDING_USER,
+			REMOVING_USER, CHANGING_PIN}; //Estados posibles de la FSM
+enum events{INPUT_EV, TIMER_EV, KEYCARD_EV};
+
+
+//UserData contiene toda la informacion necesaria que necesita una rutina para manejar
+//un evento que se saco de la cola.
+typedef struct{
+   char recieved_ID[ID_LENGTH]; //String con el ID recibido del usuario.
+   char recieved_PIN[PIN_LENGTH]; //String con el PIN recibido del usuario.
+   bool timers[NUM_TIMERS]; //Arreglo que indica que timers expiraron.
+}UserData_t;
+
+//Tipo de datos que ses almacena en la cola de eventos.
+typedef struct{
+   char ev_name; //Tipo de evento recibido.
+   UserData_t * data; //Informacion necesaria para atender el evento.
+}event_t;
+
+struct state;
+typedef struct state (*StateRoutinePtr_t)(UserData_t *);
+
+typedef struct state{
+   unsigned char name_of_state;
+   StateRoutinePtr_t Routines[NUM_EVENTS]; //La cantidad de rutinas es igual a la cantidad de eventos possibles.
+}state_t;
+
+#endif /* FSM_H_ */
