89f5c8a5e82d9128444af9a4204d0302859037d2
LuFernandez <lu.gfernandez@gmail.com>
2019-08-28 20:29:49 -0300

    cambie de lugar defines, y cambie enum por typedef enum para poder usarlos

--- projectsTP1/workspace/Baliza/source/FSM.h
+++ projectsTP1/workspace/Baliza/source/FSM.h
@@ -11,16 +11,14 @@
 #ifndef FSM_H_
 #define FSM_H_
 
-//Defincion de constantes y parametros
-//#define NUM_TIMERS 2 //Numero de timers que se utilizan.
+//Definicion de constantes y parametros
 #define ID_LENGTH 8 //Longitud maxima del ID del usuario.
 #define PIN_LENGTH 5 //Longitud maxima del PIN del usuario.
-//#define NUM_EVENTS 3  //Cantidad de eventos posibles.
 
-enum timers{INACTIVITY, DISPLAY, NUM_TIMERS}; //Timers utilizados
-enum states{WAITING_ID, RECEIVING_ID, RECEIVING_PIN, ACCESS_GRANTED, ADMIN_MODE, ADDING_USER,
-			REMOVING_USER, CHANGING_PIN, NUM_STATES}; //Estados posibles de la FSM
-enum events{INPUT_EV, TIMER_EV, KEYCARD_EV, NUM_EVENTS};
+typedef enum {INACTIVITY, DISPLAY, NUM_TIMERS}timer_name; //Timers utilizados
+typedef enum {WAITING_ID, RECEIVING_ID, RECEIVING_PIN, ACCESS_GRANTED, ADMIN_MODE, ADDING_USER,
+			REMOVING_USER, CHANGING_PIN, NUM_STATES}state_name; //Estados posibles de la FSM
+typedef enum {INPUT_EV, TIMER_EV, KEYCARD_EV, NUM_EVENTS}ev_name;
 
 
 //UserData contiene toda la informacion necesaria que necesita una rutina para manejar
@@ -31,9 +29,9 @@ typedef struct{
    bool timers[NUM_TIMERS]; //Arreglo que indica que timers expiraron.
 }UserData_t;
 
-//Tipo de datos que ses almacena en la cola de eventos.
+//Tipo de datos que se almacena en la cola de eventos.
 typedef struct{
-   char ev_name; //Tipo de evento recibido.
+	ev_name name; //Tipo de evento recibido.
    UserData_t * data; //Informacion necesaria para atender el evento.
 }event_t;
 
@@ -41,8 +39,8 @@ struct state;
 typedef struct state (*StateRoutinePtr_t)(UserData_t *);
 
 typedef struct state{
-   unsigned char name_of_state;
-   StateRoutinePtr_t Routines[NUM_EVENTS]; //La cantidad de rutinas es igual a la cantidad de eventos possibles.
+	state_name name;
+   StateRoutinePtr_t Routines[NUM_EVENTS]; //La cantidad de rutinas es igual a la cantidad de eventos posibles.
 }state_t;
 
 #endif /* FSM_H_ */
