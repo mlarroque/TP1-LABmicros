a7c99374d9ff5b78f3c23e976d336babc5a85d2d
Manuel Mollon <mmollon@itba.edu.ar>
2019-09-03 18:25:47 -0300

    Update App.c

--- projectsTP1/workspace/Baliza/source/App.c
+++ projectsTP1/workspace/Baliza/source/App.c
@@ -8,12 +8,10 @@
  * INCLUDE HEADER FILES
  ******************************************************************************/
 
+#include <stdbool.h>
 #include "FSM.h"
-#include "display.h"
-#include "timer.h"
-
-#define DELAY_BASE 900000UL
-#define DELAY_LIMIT_CONST 10
+#include "queue.h"
+#include "dataBase.h"
 
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
@@ -25,6 +23,21 @@
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
+// FSM.h
+FSM_t fsm;
+state_t nextState;
+UserData_t userData;
+// queue.h
+event_t newEvent;
+queue_t queue;
+// dataBase.h
+dataBase_t users;
+// App.c
+bool chaningState = false;
+// queues
+timerQueue_t timerQueue;
+magnetLectorQueue_t magnetLectorQueue;
+encoderQueue_t encoderQueue;
 
 /*******************************************************************************
  *******************************************************************************
@@ -35,16 +48,42 @@
 /* Función que se llama 1 vez, al comienzo del programa */
 void App_Init (void)
 {
-    InitializeDisplay();
-    PrintMessage("larroqueeee", true);
+	// IO Initialization
+    InitializeTimers();
+
+    // FSM/Queue Initialization
+    nextState.name = MENU;
+    nextState.routines[INPUT_EV] = &MinputEvHandler;
+    nextState.routines[TIMER_EV] = &MtimerEvHandler;
+    nextState.routines[KEYCARD_EV] = &MkeycardEvHandler;
+    fsm.currentState = nextState;
+    queue.top = -1;
 }
 
 /* Función que se llama constantemente en un ciclo infinito */
 void App_Run (void)
 {
-	if(GetNeedUpdate())
-	{
-		UpdateDisplay();
+	organizeEvents(&queue, &timerQueue, &magnetLectorQueue, &encoderQueue);
+	newEvent = popEvent(&queue);
+	switch(newEvent.name){
+		case INPUT_EV:
+			newState = (fsm.currentState.routines[INPUT_EV])(&userData);
+			changingState = true;
+			break;
+		case TIMER_EV:
+			newState = (fsm.currentState.routines[TIMER_EV])(&userData);
+			changingState = true;
+			break;
+		case KEYCARD_EV:
+			newState = (fsm.currentState.routines[KEYCARD_EV])(&userData);
+			changingState = true;
+			break;
+		default:
+			break;
+	}
+	if(changingState){
+		fsm.currentState = newState;
+		changingState = false;
 	}
 }
 
@@ -54,9 +93,6 @@ void App_Run (void)
                         LOCAL FUNCTION DEFINITIONS
  *******************************************************************************
  ******************************************************************************/
-
-
-
 
 /*******************************************************************************
  ******************************************************************************/
