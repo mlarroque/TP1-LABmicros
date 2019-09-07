0f78a077b8f5d8711fc218cf0736df31c79abbb8
Manuel Mollon <mmollon@itba.edu.ar>
2019-09-06 21:50:07 -0300

    compila fsm

---
+++ projectsTP1/workspace/Baliza/Debug/source/stateReceivingPIN.d
@@ -0,0 +1,30 @@
+source/stateReceivingPIN.o source/stateReceivingPIN.d: \
+ ../source/stateReceivingPIN.c ../source/stateReceivingPIN.h \
+ ../source/userData.h ../source/timer.h ../source/encoder.h \
+ ../source/magnetCardLector.h ../source/FSM.h ../source/queue.h \
+ ../source/stateBlocked.h ../source/stateMenu.h \
+ ../source/stateUserApproved.h ../source/display.h ../source/dataBase.h
+
+../source/stateReceivingPIN.h:
+
+../source/userData.h:
+
+../source/timer.h:
+
+../source/encoder.h:
+
+../source/magnetCardLector.h:
+
+../source/FSM.h:
+
+../source/queue.h:
+
+../source/stateBlocked.h:
+
+../source/stateMenu.h:
+
+../source/stateUserApproved.h:
+
+../source/display.h:
+
+../source/dataBase.h:
